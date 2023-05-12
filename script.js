12a2f4c49aea5af8fe3a087c54a311e80800898e
William Karliner <120746874+Lochroach@users.noreply.github.com>
2023-05-12 16:02:19 -0600

    Added if...else for draw in playRound
    edited parameters of all functions

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const getComputerChoice = (choice) => {
+const getComputerChoice = (compChoice) => {
     const choiceArray = ['rock', 'paper', 'scissors'];
     const randomComputerChoice = (Math.floor(Math.random() * choiceArray.length));
-    return choice = randomComputerChoice;
+    return compChoice = randomComputerChoice;
 }
 
-const getPlayerChoice = () => {
+const getPlayerChoice = (playerChoice) => {
     prompt('Rock, Paper, or Scissors?')
 }
 
-const playRound = () => {
-    
+const playRound = (final) => {
+    for (rock = 0; paper = 1; scissors = 2) {
+        if (paper === paper) {
+            console.log('Draw');
+        } else if (rock === rock) {
+            console.log('Draw');
+        } else if (scissors === scissors) {
+            console.log('Draw');
+        } else {}
+    }
+    console.log(rock);
 }
-
-console.log(getComputerChoice());
-
