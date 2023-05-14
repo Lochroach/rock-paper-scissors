7d867f2524b9e37a5e8d4a2bb9973c16315a9d3b
William Karliner <120746874+Lochroach@users.noreply.github.com>
2023-05-14 16:26:49 -0600

    Deleted playRound code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const choiceArray = ['rock', 'paper', 'scissors'];
+
+
 const getComputerChoice = (compChoice) => {
-    const choiceArray = ['rock', 'paper', 'scissors'];
     const randomComputerChoice = (Math.floor(Math.random() * choiceArray.length));
     return compChoice = randomComputerChoice;
 }
@@ -9,23 +11,6 @@ const getPlayerChoice = (playerChoice) => {
 }
 
 const playRound = (getPlayerChoice, getComputerChoice) => {
-    for (rock = 0; paper = 1; scissors = 2) {
-        if (paper === paper) {
-            console.log('Draw');
-        } else if (rock === rock) {
-            console.log('Draw');
-        } else if (scissors === scissors) {
-            console.log('Draw');
-        } else if (paper > rock) {
-            console.log('You win! Paper beats Rock.');
-        } else if (scissors > paper) {
-            console.log('You Win! Scissors beats Paper.');
-        } else if (rock < scissors) {
-            console.log('You Win! Rock beats Scissors.')
-        }
-    }
-
     return getComputerChoice, getPlayerChoice;
 }
 
-console.log(playRound);
