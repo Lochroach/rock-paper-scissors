2d2c85bff237f4d0be145d6e15f3cfc490952678
William Karliner <120746874+Lochroach@users.noreply.github.com>
2023-05-14 13:54:36 -0600

    Created "You Win!" part of playRound function

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const getPlayerChoice = (playerChoice) => {
     prompt('Rock, Paper, or Scissors?')
 }
 
-const playRound = (final) => {
+const playRound = (getPlayerChoice, getComputerChoice) => {
     for (rock = 0; paper = 1; scissors = 2) {
         if (paper === paper) {
             console.log('Draw');
@@ -16,6 +16,16 @@ const playRound = (final) => {
             console.log('Draw');
         } else if (scissors === scissors) {
             console.log('Draw');
-        } else {}
+        } else if (paper > rock) {
+            console.log('You win! Paper beats Rock.');
+        } else if (scissors > paper) {
+            console.log('You Win! Scissors beats Paper.');
+        } else if (rock < scissors) {
+            console.log('You Win! Rock beats Scissors.')
+        }
     }
+
+    return getComputerChoice, getPlayerChoice;
 }
+
+console.log(playRound);
