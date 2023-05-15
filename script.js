328381896d73b498cad23273c12f7766c5da8fb5
William Karliner <120746874+Lochroach@users.noreply.github.com>
2023-05-14 19:03:19 -0600

    Added code for playRound fn, works now!
    for some reason the function is also logging the playerChoice
    as well as the result

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 const choiceArray = ['rock', 'paper', 'scissors'];
 
 
-const getComputerChoice = (compChoice) => {
+const getComputerChoice = (choice) => {
     const randomComputerChoice = (Math.floor(Math.random() * choiceArray.length));
-    return compChoice = randomComputerChoice;
+    return choice = randomComputerChoice;
 }
 
-const getPlayerChoice = (playerChoice) => {
-    if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
-        return playerChoice;
+const getPlayerChoice = (choice) => {
+    if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+        return choice;
     } else {
         console.log('Please make a valid choice of rock, paper, or scissors')
     }
 }
 
-const playRound = (getPlayerChoice, getComputerChoice) => {
-    return getComputerChoice, getPlayerChoice;
+const playRound = (playerChoice, computerChoice) => {
+    if (playerChoice === computerChoice) {
+        console.log('Draw');
+    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+        console.log('You Lose! Paper beats Rock.');
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+        console.log('You Lose! Scissors beat Paper.');
+    } else if (playerChoice === 'scissor' && computerChoice === 'rock') {
+        console.log('You Lose! Rock beats Scissors.');
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You Win! Scissors beat Paper.');
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You Win! Paper beats Rock.');
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You Win! Rock beats Scissors');
+    } else {
+        console.log('Could not determine winner with current argument, please try again with valid input.')
+    }
+    return computerChoice, playerChoice;
 }
 
+console.log(playRound('rock', 'rock'));
