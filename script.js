9c40adff4f839a7e4e5fa0ca701a29f5a78de4ad
William Karliner <120746874+Lochroach@users.noreply.github.com>
2023-05-15 16:49:41 -0600

    Made user input case insensitive, added for loop

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const getComputerChoice = (choice) => {
 }
 
 const getPlayerChoice = (choice) => {
-    if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+    if (choice.toLowerCase() === 'rock'.toLowerCase() || choice.toLowerCase() === 'paper'.toLowerCase() || choice.toLowerCase() === 'scissors'.toLowerCase()) {
         return choice;
     } else {
         console.log('Please make a valid choice of rock, paper, or scissors')
@@ -17,17 +17,17 @@ const getPlayerChoice = (choice) => {
 const playRound = (playerChoice, computerChoice) => {
     if (playerChoice === computerChoice) {
         console.log('Draw');
-    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+    } else if (playerChoice.toLowerCase() === 'rock'.toLowerCase() && computerChoice === 'paper') {
         console.log('You Lose! Paper beats Rock.');
-    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+    } else if (playerChoice.toLowerCase() === 'paper'.toLowerCase() && computerChoice === 'scissors') {
         console.log('You Lose! Scissors beat Paper.');
-    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+    } else if (playerChoice.toLowerCase() === 'scissors'.toLowerCase() && computerChoice === 'rock') {
         console.log('You Lose! Rock beats Scissors.');
-    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+    } else if (playerChoice.toLowerCase() === 'scissors'.toLowerCase() && computerChoice === 'paper') {
         console.log('You Win! Scissors beats Paper.');
-    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+    } else if (playerChoice.toLowerCase() === 'paper'.toLowerCase() && computerChoice === 'rock') {
         console.log('You Win! Paper beats Rock.');
-    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    } else if (playerChoice.toLowerCase() === 'rock'.toLowerCase() && computerChoice === 'scissors') {
         console.log('You Win! Rock beats Scissors');
     } else {
         console.log('Could not determine winner with current argument, please try again with valid input.')
@@ -36,8 +36,10 @@ const playRound = (playerChoice, computerChoice) => {
 }
 
 const playGame = () => {
+    for (let i = 1; i <= 5; i++) {
     const playerInput = prompt('Rock, Paper, or Scissors?');
     playRound(getPlayerChoice(playerInput), getComputerChoice());
+    }
 }
 
 playGame();
