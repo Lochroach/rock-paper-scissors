b29994e35e48a6d9c1fbe2a3e2ae1c8bd24ea80d
William Karliner <120746874+Lochroach@users.noreply.github.com>
2023-05-17 16:26:26 -0600

    Added winner fn and score keeper

--- script.js
+++ script.js
@@ -19,16 +19,22 @@ const playRound = (playerChoice, computerChoice) => {
     if (playerChoice === computerChoice) {
         console.log('Draw');
     } else if (playerChoice.toLowerCase() === 'rock'.toLowerCase() && computerChoice === 'paper') {
+        computerScore += 1;
         console.log('You Lose! Paper beats Rock.');
     } else if (playerChoice.toLowerCase() === 'paper'.toLowerCase() && computerChoice === 'scissors') {
+        computerScore += 1;
         console.log('You Lose! Scissors beat Paper.');
     } else if (playerChoice.toLowerCase() === 'scissors'.toLowerCase() && computerChoice === 'rock') {
+        computerScore += 1;
         console.log('You Lose! Rock beats Scissors.');
     } else if (playerChoice.toLowerCase() === 'scissors'.toLowerCase() && computerChoice === 'paper') {
+        playerScore += 1;
         console.log('You Win! Scissors beats Paper.');
     } else if (playerChoice.toLowerCase() === 'paper'.toLowerCase() && computerChoice === 'rock') {
+        playerScore += 1;
         console.log('You Win! Paper beats Rock.');
     } else if (playerChoice.toLowerCase() === 'rock'.toLowerCase() && computerChoice === 'scissors') {
+        playerScore += 1;
         console.log('You Win! Rock beats Scissors');
     } else {
         console.log('Could not determine winner with current argument, please try again with valid input.')
@@ -36,12 +42,28 @@ const playRound = (playerChoice, computerChoice) => {
     return computerChoice, playerChoice;
 }
 
-const playGame = (playerScore, computerScore) => {
-    for (let i = 1; i <= 5; i++) {
+const playGame = () => {
+    for (let i = 0; i < 5; i++) {
     const playerInput = prompt('Rock, Paper, or Scissors?');
     playRound(getPlayerChoice(playerInput), getComputerChoice());
     }
-    return playerScore, computerScore;
+    winner();
+}
+
+const winner = () => {
+    if (playerScore > computerScore) {
+        console.log('---------------------');
+        console.log('You won the game! Congrats!');
+    } else if (computerScore > playerScore) {
+        console.log('---------------------');
+        console.log('The computer won the game, sorry eh!');
+    } else if (playerScore === computerScore) {
+        console.log('---------------------');
+        console.log('It\'s a tie!');
+    } else {
+        console.log('---------------------');
+        console.log('Winner could not be determined.');
+    }
 }
 
 playGame();
